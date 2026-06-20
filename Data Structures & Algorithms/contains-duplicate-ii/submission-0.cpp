455aa5e4e0848e51e5d016d1f5cd01c548dcf46c
VIshal <79686922+wearetec9@users.noreply.github.com>
2026-06-20 21:45:39 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        std::unordered_map<int, int> mp;
+        bool state = false;
+        for (int i = 0; i < nums.size(); i++) {
+            int curr = nums[i];
+            if(mp.contains(curr)) {
+                int j = mp[curr];
+                if (std::abs(i - j) <= k) {
+                    state = true;
+                }
+            }
+            mp[curr] = i;
+        }
+        return state;
+    }
+};
